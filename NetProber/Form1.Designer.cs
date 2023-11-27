13cb338cc66a520e0c55d95750a8e6705332c08b
rozen <rozenberg.kim@gmail.com>
2023-11-27 20:46:55 +0200

    Include all the classes and a button for opening the shape file (non-functional). Additionally, add and incorporate all the properties of the 'VertexClass' class.

--- NetProber/Form1.Designer.cs
+++ NetProber/Form1.Designer.cs
@@ -28,13 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.shapesFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.shapesFileToolStripMenuItem});
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // shapesFileToolStripMenuItem
+            // 
+            this.shapesFileToolStripMenuItem.Name = "shapesFileToolStripMenuItem";
+            this.shapesFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.shapesFileToolStripMenuItem.Text = "Shapes file";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem shapesFileToolStripMenuItem;
     }
 }
 
---
+++ NetProber/VertexClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetProber
+{
+    internal class VertexClass
+    {
+        public enum Type
+        {
+            LINE,
+            ARC
+        }
+        public Type shapeType {  get; set; }
+
+        public PointF startX { get; set; }
+        public PointF startY { get; set; }
+        public PointF endX { get; set; }
+        public PointF endY { get; set; }
+        public PointF centerX { get; set; }
+        public PointF centerY { get; set; }
+        public double raduis {  get; set; }
+        public int width { get; set; }
+
+        public VertexClass(Type shapeType ,PointF startX, PointF startY, PointF endX, PointF endY, PointF centerX, PointF centerY, double raduis, int width)
+        {
+            this.shapeType = shapeType;
+            this.startX = startX;
+            this.startY = startY;
+            this.endX = endX;
+            this.endY = endY;
+            this.centerX = centerX;
+            this.centerY = centerY;
+            this.raduis = raduis;
+            this.width = width;
+        }
+    }
+}
