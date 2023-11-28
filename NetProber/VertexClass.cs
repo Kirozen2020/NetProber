509f4fc1b1ccc2a484140efc73e91fb5e698ad62
rozen <rozenberg.kim@gmail.com>
2023-11-28 15:31:15 +0200

    Modify the VertexClass.

--- NetProber/VertexClass.cs
+++ NetProber/VertexClass.cs
@@ -11,24 +11,26 @@ namespace NetProber
         }
         public Type shapeType {  get; set; }
 
-        public PointF startX { get; set; }
-        public PointF startY { get; set; }
-        public PointF endX { get; set; }
-        public PointF endY { get; set; }
-        public PointF centerX { get; set; }
-        public PointF centerY { get; set; }
+        public PointF start { get; set; }
+        public PointF end { get; set; }
+        public PointF center { get; set; }
         public double raduis {  get; set; }
         public int width { get; set; }
 
-        public VertexClass(Type shapeType ,PointF startX, PointF startY, PointF endX, PointF endY, PointF centerX, PointF centerY, double raduis, int width)
+        public VertexClass(Type shapeType, PointF start, PointF end, int width)
         {
             this.shapeType = shapeType;
-            this.startX = startX;
-            this.startY = startY;
-            this.endX = endX;
-            this.endY = endY;
-            this.centerX = centerX;
-            this.centerY = centerY;
+            this.start = start;
+            this.end = end;
+            this.width = width;
+        }
+
+        public VertexClass(Type shapeType, PointF start, PointF end, PointF center, double raduis, int width)
+        {
+            this.shapeType = shapeType;
+            this.start = start;
+            this.end = end;
+            this.center = center;
             this.raduis = raduis;
             this.width = width;
         }
