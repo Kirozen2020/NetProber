e0cba42766e2958ea5e7dd09f8f169685f491f54
rozen <rozenberg.kim@gmail.com>
2023-11-28 14:55:43 +0200

    Complete the implementation of the FileReader class.

--- NetProber/FileReader.cs
+++ NetProber/FileReader.cs
@@ -1,8 +1,30 @@
-﻿
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
 namespace NetProber
 {
     internal class FileReader
     {
+        private string filePath;
 
+        public FileReader(string path)
+        {
+            this.filePath = path;
+        }
+
+        public List<string> TextReader()
+        {
+            List<string> lines = new List<string>();
+            try
+            {
+                lines = File.ReadAllLines(this.filePath).ToList();
+            }
+            catch
+            {
+                return null;
+            }
+            return lines;
+        }
     }
 }
